34d00ca838be7f6f177035a0256e2e3ba9a73daf
Vadim Kislov <kislov.v@gmail.com>
2022-08-31 13:00:32 +0300

    Initial Commit

---
+++ UIAnimationFigure/UIAnimationFigureApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIAnimationFigureApp.swift
+//  UIAnimationFigure
+//
+//  Created by Kislov Vadim on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIAnimationFigureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
